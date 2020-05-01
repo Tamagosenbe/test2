bf0d86be3ed81d833cd79219e95b3b93e9eb282e
Tamagosenbe <tamamukakashi@gmail.com>
2020-05-01 14:44:48 +0900

    3

--- test2.cpp
+++ test2.cpp
@@ -1,1 +1,14 @@
 //aaaaaaa
+#include <iostream>
+
+int main(){
+    int a [10];
+    a[0] = 0;
+    a[1] = 1;
+    for (int i = 2; i < 10; ++i){
+        a[i] = a[i - 2] + a[i - 1];
+    }
+    return 0;
+    }
+
+    
